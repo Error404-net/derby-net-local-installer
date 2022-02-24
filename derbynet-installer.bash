0ca7f47239fcb6efcb2bcd41852be562a2c0a21d
PorkChopExpress86 <29725019+PorkChopExpress86@users.noreply.github.com>
2022-02-24 10:01:10 -0600

    Create install script

---
+++ derbynet-installer.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# assumes that we are root
+
+# Update all packages
+apt-get update && apt-get -y dist-upgrade && apt-get -y dist-upgrade
+
+apt-get install -y apt-transport-https
+
+wget -q -O- https://jeffpiazza.org/derbynet/debian/jeffpiazza_derbynet.gpg | \
+tee /usr/share/keyrings/derbynet-archive-keyring.gpg
+
+echo "deb [arch=all signed-by=/usr/share/keyrings/derbynet-archive-keyring.gpg] " \
+" https://jeffpiazza.org/derbynet/debian stable main" | \
+tee /etc/apt/sources.list.d/derbynet.list > /dev/null
+
+apt-get update
+
+# install derbynet server
+apt-get install -y derbynet-server
+
+# remove apache if installed since derbynet is dependet on nginx
+apt-get remove -y apache2
+
+
+# install and config certbot for ssl cert
