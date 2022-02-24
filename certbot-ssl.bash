2a607a1d0c5545ec05683846cd62b0f5fad9ca58
Porkchopexpress86 <blake.b.8726@gmail.com>
2022-02-24 12:14:29 -0600

    Create another script for installing certbot via snaps to handle ssl/tls connections with nginx.

---
+++ certbot-ssl.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# install snapd
+apt-get update
+apt-get install -y snapd
+
+snap install core
+snap refresh core
+
+# install certbot
+apt remove -y certbot
+snap install --classic certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
+
+## request a tls/ssl certifiacte using certbot
+# request a certificate and automatically configure it on nginx
+certbot --nginx
+
+# request a certificate without configuring nginx
+# certbot certonly --nginx
+
